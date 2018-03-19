e7753c25516eef5ba7f8717f64647cdf35910b9d
NickRowlandson <nicholasrowlandson@gmail.com>
2018-03-18 21:50:04 -0400

    server

---
+++ src/server/index.js
@@ -0,0 +1,15 @@
+// Get dependencies
+const express = require('express');
+const express = require('express');
+
+const publicweb = process.env.PUBLICWEB  '.distpublicweb';
+const app = express();
+
+app.use(express.static(publicweb));
+console.log(`serving ${publicweb}`);
+app.get('', (req, res) = {
+  res.sendFile(`index.html`, { root publicweb });
+});
+
+const port = process.env.SERVER_PORT  '3000';
+app.listen(port, () = console.log(`API running on localhost${port}`));
